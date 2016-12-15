c3d246cf5adf370dc0a9afaf2522e6c26a5f6b75
LightAir <e.konkin@cb-cp.ru>
2016-12-15 14:13:51 +0300

    Fixed typos. Powered by PEP8 standard. Fixed shadowing names defined

--- clickhouse.py
+++ clickhouse.py
@@ -1,8 +1,8 @@
-import requests
-import urllib
-import utils
-import sys
 import logging
+
+import requests
+
+import utils
 
 config = utils.get_config()
 CH_HOST = config['clickhouse']['host']
@@ -12,11 +12,11 @@ CH_VISITS_TABLE = config['clickhouse']['visits_table']
 CH_HITS_TABLE = config['clickhouse']['hits_table']
 CH_DATABASE = config['clickhouse']['database']
 
-
 logger = logging.getLogger('logs_api')
 
+
 def get_clickhouse_data(query, host=CH_HOST):
-    '''Returns ClickHouse response'''
+    """Returns ClickHouse response"""
     logger.debug(query)
     if (CH_USER == '') and (CH_PASSWORD == ''):
         r = requests.post(host, data=query)
@@ -29,14 +29,14 @@ def get_clickhouse_data(query, host=CH_HOST):
 
 
 def upload(table, content, host=CH_HOST):
-    '''Uploads data to table in ClickHous'''
+    """Uploads data to table in ClickHouse"""
     query_dict = {
-             'query': 'INSERT INTO ' + table + ' FORMAT TabSeparatedWithNames '
-        }
+        'query': 'INSERT INTO ' + table + ' FORMAT TabSeparatedWithNames '
+    }
     if (CH_USER == '') and (CH_PASSWORD == ''):
         r = requests.post(host, data=content, params=query_dict)
     else:
-        r = requests.post(host, data=content, params=query_dict, 
+        r = requests.post(host, data=content, params=query_dict,
                           auth=(CH_USER, CH_PASSWORD))
     result = r.text
     if r.status_code == 200:
@@ -46,7 +46,7 @@ def upload(table, content, host=CH_HOST):
 
 
 def get_source_table_name(source, with_db=True):
-    '''Returns table name in database'''
+    """Returns table name in database"""
     if source == 'hits':
         if with_db:
             return '{db}.{table}'.format(db=CH_DATABASE, table=CH_HITS_TABLE)
@@ -60,18 +60,18 @@ def get_source_table_name(source, with_db=True):
 
 
 def get_tables():
-    '''Returns list of tables in database'''
-    return get_clickhouse_data('SHOW TABLES FROM {db}'.format(db=CH_DATABASE))\
+    """Returns list of tables in database"""
+    return get_clickhouse_data('SHOW TABLES FROM {db}'.format(db=CH_DATABASE)) \
         .strip().split('\n')
 
 
 def is_table_present(source):
-    '''Returns whether table for data is already present in database'''
+    """Returns whether table for data is already present in database"""
     return get_source_table_name(source, with_db=False) in get_tables()
 
 
 def get_ch_field_name(field_name):
-    '''Converts Logs API parameter name to ClickHouse column name'''
+    """Converts Logs API parameter name to ClickHouse column name"""
     prefixes = ['ym:s:', 'ym:pv:']
     for prefix in prefixes:
         field_name = field_name.replace(prefix, '')
@@ -79,14 +79,14 @@ def get_ch_field_name(field_name):
 
 
 def drop_table(source):
-    '''Drops table in ClickHouse'''
+    """Drops table in ClickHouse"""
     query = 'DROP TABLE IF EXISTS {table}'.format(
         table=get_source_table_name(source))
     get_clickhouse_data(query)
 
 
 def create_table(source, fields):
-    '''Creates table in ClickHouse for hits/visits with particular fields'''
+    """Creates table in ClickHouse for hits/visits with particular fields"""
     tmpl = '''
         CREATE TABLE {table_name} (
             {fields}
@@ -96,23 +96,22 @@ def create_table(source, fields):
     field_statements = []
 
     table_name = get_source_table_name(source)
+
+    engine = 'Log'
+
     if source == 'hits':
         if ('ym:pv:date' in fields) and ('ym:pv:clientID' in fields):
             engine = 'MergeTree(Date, intHash32(ClientID), (CounterID, Date, intHash32(ClientID)), 8192)'
-        else:
-            engine = 'Log'
 
     if source == 'visits':
         if ('ym:s:date' in fields) and ('ym:s:clientID' in fields):
             engine = 'MergeTree(Date, intHash32(ClientID), (CounterID, Date, intHash32(ClientID)), 8192)'
-        else:
-            engine = 'Log'
 
     ch_field_types = utils.get_ch_fields_config()
     ch_fields = sorted(map(get_ch_field_name, fields))
     for i in range(len(fields)):
-        field_statements.append(field_tmpl.format(name= ch_fields[i],
-            type=ch_field_types[fields[i]]))
+        field_statements.append(field_tmpl.format(name=ch_fields[i],
+                                                  type=ch_field_types[fields[i]]))
 
     query = tmpl.format(table_name=table_name,
                         engine=engine,
@@ -122,7 +121,7 @@ def create_table(source, fields):
 
 
 def save_data(source, fields, data):
-    '''Inserts data into ClickHouse table'''
+    """Inserts data into ClickHouse table"""
     if not is_table_present(source):
         create_table(source, fields)
 
@@ -130,7 +129,7 @@ def save_data(source, fields, data):
 
 
 def is_data_present(start_date_str, end_date_str, source):
-    '''Returns whether there is a records in database for particular date range and source'''
+    """Returns whether there is a records in database for particular date range and source"""
     if not is_table_present(source):
         return False
 
@@ -145,4 +144,3 @@ def is_data_present(start_date_str, end_date_str, source):
 
     visits = get_clickhouse_data(query, CH_HOST)
     return visits != ''
-
--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
-import json
 import argparse
+import json
+
 import requests
 
 DATE_FORMAT = '%Y-%m-%d'
+
 
 class Structure:
     def __init__(self, **kwds):
@@ -16,17 +18,15 @@ class Structure:
 
 
 def validate_user_request(user_request):
-    '''Validates initial user request'''
+    """Validates initial user request"""
     assert user_request.source in ['hits', 'visits'], 'Invalid source'
 
 
 def validate_cli_options(options):
-    '''Validates command line options'''
-    assert options.source is not None, \
-        'Source must be specified in CLI options'
+    """Validates command line options"""
+    assert options.source is not None, 'Source must be specified in CLI options'
     if options.mode is None:
-        assert (options.start_date is not None) \
-            and (options.end_date is not None), \
+        assert (options.start_date is not None) and (options.end_date is not None), \
             'Dates or mode must be specified'
     else:
         assert options.mode in ['history', 'regular', 'regular_early'], \
@@ -34,19 +34,19 @@ def validate_cli_options(options):
 
 
 def get_cli_options():
-    '''Returns command line options'''
+    """Returns command line options"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-start_date', help = 'Start of period')
-    parser.add_argument('-end_date', help = 'End of period')
-    parser.add_argument('-mode', help = 'Mode (one of [history, reqular, regular_early])')
-    parser.add_argument('-source', help = 'Source (hits or visits)')
+    parser.add_argument('-start_date', help='Start of period')
+    parser.add_argument('-end_date', help='End of period')
+    parser.add_argument('-mode', help='Mode (one of [history, regular, regular_early])')
+    parser.add_argument('-source', help='Source (hits or visits)')
     options = parser.parse_args()
     validate_cli_options(options)
     return options
 
 
 def get_counter_creation_date(counter_id, token):
-    '''Returns create date for counter'''
+    """Returns create date for counter"""
     host = 'https://api-metrika.yandex.ru'
     url = '{host}/management/v1/counter/{counter_id}?oauth_token={token}' \
         .format(counter_id=counter_id, token=token, host=host)
@@ -58,7 +58,7 @@ def get_counter_creation_date(counter_id, token):
 
 
 def get_config():
-    '''Returns user config'''
+    """Returns user config"""
     with open('./configs/config.json') as input_file:
         config = json.loads(input_file.read())
 
@@ -70,8 +70,7 @@ def get_config():
 
 
 def get_ch_fields_config():
-    '''Returns config for ClickHouse columns\'s datatypes'''
+    """Returns config for ClickHouse columns\'s datatypes"""
     with open('./configs/ch_types.json') as input_file:
         ch_field_types = json.loads(input_file.read())
     return ch_field_types
-
